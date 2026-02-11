5bb739ef09e607f722dd23d3cb5109c42f4de8a7
Kyle Son <sonkyle0420@gmail.com>
2026-02-11 00:03:34 -0800

    Added images and the input section

---
+++ public/scripts/server.js
@@ -0,0 +1,4 @@
+const imageNames = [
+    'astra', 'breach', 'brimstone', 'chamber', 'clove', 'cypher', 'deadlock', 'fade', 'gekko', 'harbor', 'iso', 'jett', 'kayo', 'killjoy', 'neon', 'omen', 'phoenix', 
+    'raze', 'reyna', 'sage', 'skye', 'sova', 'tejo', 'veto', 'viper', 'vyse', 'yoru'
+];
