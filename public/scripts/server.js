931a27065a49f9c9b64b224e0a12b3a837c31be0
Kyle Son <sonkyle0420@gmail.com>
2026-02-16 15:36:05 -0800

    implemented button functionality, added agent icons to the image folder

--- public/scripts/server.js
+++ public/scripts/server.js
@@ -1,4 +1,59 @@
+let agentIndex = 0;
+const img = document.getElementById('agent-image');
+
 const imageNames = [
     'astra', 'breach', 'brimstone', 'chamber', 'clove', 'cypher', 'deadlock', 'fade', 'gekko', 'harbor', 'iso', 'jett', 'kayo', 'killjoy', 'neon', 'omen', 'phoenix', 
-    'raze', 'reyna', 'sage', 'skye', 'sova', 'tejo', 'veto', 'viper', 'vyse', 'yoru'
+    'raze', 'reyna', 'sage', 'skye', 'sova', 'tejo', 'veto', 'viper', 'vyse', 'waylay', 'yoru'
 ];
+
+function getNextAgentImage() {
+    agentIndex++;
+    if (agentIndex > imageNames.length -1){
+        agentIndex = imageNames.length -1;
+        document.getElementById('submit-btn').style.display = 'none';
+        document.getElementById('back-btn').style.display = 'none';
+        document.getElementById('skip-btn').style.display = 'none';
+        document.getElementById('agent-image').style.display = 'none';
+        document.getElementById('input-field').style.display = 'none';
+    }
+    if (img) {
+        img.src = `img/splash/${imageNames[agentIndex]}.png`;
+        img.alt = imageNames[agentIndex];
+    }
+}
+
+function getPreviousAgentImage() {
+    agentIndex--;
+    if (agentIndex < 0){
+        agentIndex = 0;
+    }
+    if (img) {
+        img.src = `img/splash/${imageNames[agentIndex]}.png`;
+        img.alt = imageNames[agentIndex];
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const submitBtn = document.getElementById('submit-btn');
+    const backBtn = document.getElementById('back-btn');
+    const skipBtn = document.getElementById('skip-btn');
+    if (submitBtn) {
+        submitBtn.addEventListener('click', (e) => {
+            e.preventDefault();
+            getNextAgentImage();
+            
+        });
+    }
+    if (backBtn) {
+        backBtn.addEventListener('click', (e) => {
+            e.preventDefault();
+            getPreviousAgentImage();
+        });
+    }
+    if (skipBtn) {
+        skipBtn.addEventListener('click', (e) => {
+            e.preventDefault();
+            getNextAgentImage();
+        }); 
+    }
+});
